d85bac57821246db3ee71ff703ad70232b558a17
aula1 <aula1@gmail.com>
2024-12-29 23:21:55 -0300

    tela login sem medkit

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ app.use(cors());
 
 const SECRET_KEY = 'segredo';
 
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
